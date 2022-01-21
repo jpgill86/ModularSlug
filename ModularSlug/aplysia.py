1949b24617f3bf6768c22dfb48b77a9572840108
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2022-01-21 17:13:40 -0500

    Check for incorrect neural and muscle model types in Aplysia class

--- ModularSlug/aplysia.py
+++ ModularSlug/aplysia.py
@@ -1,12 +1,45 @@
 import numpy as np
+from .neuralmodels import GenericNeuralModel
+from .musclemodels import GenericMuscleModel
 
 
 class Aplysia:
 
     def __init__(self, n_steps, neural_model, muscle_model):
+        self._neural_model = None
+        self._muscle_model = None
+
         self.n_steps = n_steps
         self.neural_model = neural_model
         self.muscle_model = muscle_model
+
+    @property
+    def neural_model(self):
+        # defining this property is required for creating its custom setter
+        return self._neural_model
+
+    @neural_model.setter
+    def neural_model(self, obj):
+        # ensure neural_model can only be set to an instance of a subclass of GenericNeuralModel or to None
+        if not isinstance(obj, (GenericNeuralModel, type(None))):
+            raise TypeError('tried to set neural_model to an incompatible '
+                            f'object type: {obj.__class__.__name__}')
+
+        self._neural_model = obj
+
+    @property
+    def muscle_model(self):
+        # defining this property is required for creating its custom setter
+        return self._muscle_model
+
+    @muscle_model.setter
+    def muscle_model(self, obj):
+        # ensure muscle_model can only be set to an instance of a subclass of GenericMuscleModel or to None
+        if not isinstance(obj, (GenericMuscleModel, type(None))):
+            raise TypeError('tried to set muscle_model to an incompatible '
+                            f'object type: {obj.__class__.__name__}')
+
+        self._muscle_model = obj
 
     def run(self):
         for i in range(self.n_steps):
