f6c7896319a4d4b3d5e74628622a917b39e51e7d
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2022-01-21 18:05:06 -0500

    Update example muscle calculations to use neural outputs

--- ModularSlug/musclemodels/mymusclemodel.py
+++ ModularSlug/musclemodels/mymusclemodel.py
@@ -5,12 +5,13 @@ from .genericmusclemodel import GenericMuscleModel
 class MyMuscleModel(GenericMuscleModel):
 
     def step(self):
-        self.t += 1
 
-        # do some calculations using self.params and self.x[self.t-1]
+        # do some calculations using self.params, self.neural_outputs, and self.muscle_outputs
 
         # store this iteration's outputs in x
         # - just making up numbers up here
-        self.x[self.t]['I2'] = 2 * self.t
-        self.x[self.t]['I3'] = 3 * self.t
-        self.x[self.t]['I4'] = 4 * self.t
+        self.x[self.t + 1]['I2'] = 0.5 * self.muscle_outputs['I2'] + self.neural_outputs['B31']
+        self.x[self.t + 1]['I3'] = 0.5 * self.muscle_outputs['I3'] + self.neural_outputs['B3']
+        self.x[self.t + 1]['I4'] = 0.5 * self.muscle_outputs['I4'] + self.neural_outputs['B8']
+
+        self.t += 1
